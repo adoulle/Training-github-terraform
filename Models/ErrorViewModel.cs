04396d28e0a8341c1c23ca08b1197e30be67314d
Alexandre Doulle <alexandre.doulle@wolterskluwer.com>
2025-05-26 17:15:59 +0200

    feat: test Related work items: #XXXXXX

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Training_github_terraform.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
